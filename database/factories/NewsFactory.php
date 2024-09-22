89038e03c19139addc45609a991a46a8715a0b0c
Dev <dev@devsandbox.com>
2024-09-22 15:24:44 +0500

    Adds endpoint to fetch articels with pagination. Tests also applied

---
+++ database/factories/NewsFactory.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace Database\Factories;
+
+use App\Models\News;
+use Illuminate\Database\Eloquent\Factories\Factory;
+
+class NewsFactory extends Factory
+{
+    protected $model = News::class;
+
+    public function definition()
+    {
+        return [
+            'title' => $this->faker->sentence,
+            'description' => $this->faker->paragraph,
+            'url' => $this->faker->url,
+            'source' => $this->faker->word,
+            'published_at' => $this->faker->dateTimeThisYear,
+        ];
+    }
+}
--- routes/api.php
+++ routes/api.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\NewsController;
 use App\Http\Controllers\PasswordResetController;
@@ -10,8 +11,8 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
-Route::post('register', [AuthController::class, 'register']);
-Route::post('login', [AuthController::class, 'login']);
+Route::post('register', [AuthController::class, 'register'])->name('register');
+Route::post('login', [AuthController::class, 'login'])->name('login');
 Route::post('logout', [AuthController::class, 'logout'])->middleware('auth:sanctum');
 Route::post('password/forgot', [PasswordResetController::class, 'sendResetLink'])->name('password-forgot');
 Route::post('password/reset', [PasswordResetController::class, 'resetPassword'])->name('password-reset');
@@ -20,3 +21,7 @@ Route::post('password/reset', [PasswordResetController::class, 'resetPassword'])
 Route::get('fetch-news', [NewsController::class, 'fetchNewsFromNewsApi']);
 // NYT API
 Route::get('fetch-nyt-news', [NewsController::class, 'fetchNewsFromNYT']);
+
+Route::middleware('auth:sanctum')->group(function () {
+    Route::get('/articles', [ArticleController::class, 'getPaginatedArticles']);
+}); 
